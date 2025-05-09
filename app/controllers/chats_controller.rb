a6caacdf7cf1521679efb78001cd545deb3303f0
freebeiro <francisco.ribeiro@homeflow.co.uk>
2025-05-09 16:51:43 +0100

    Implement message creation and Turbo Stream broadcasting

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,4 +3,34 @@ class ChatsController < ApplicationController
     @conversation = Conversation.last || Conversation.create
     @messages = @conversation.messages.order(created_at: :asc)
   end
+
+  def create
+    @conversation = Conversation.find(params[:conversation_id])
+    @message = @conversation.messages.build(message_params)
+    @message.sender_type = "User" # Assuming user is sending the message
+
+    if @message.save
+      # Broadcasting will be handled by the model's callbacks after `broadcasts_to` is added.
+      # For now, we just need to respond to the turbo-frame request.
+      render turbo_stream: turbo_stream.replace(
+        "new_message", # The ID of the turbo-frame to replace
+        partial: "messages/form",
+        locals: { conversation: @conversation, message: Message.new } # Reset form
+      )
+    else
+      # Handle validation errors, perhaps re-render the form with errors
+      # For PoC, keeping it simple. If save fails, the form won't reset.
+      render turbo_stream: turbo_stream.replace(
+        "new_message",
+        partial: "messages/form",
+        locals: { conversation: @conversation, message: @message } # Show errors
+      )
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :conversation
+  after_create_commit { broadcast_append_to conversation, target: "messages", partial: "messages/message", locals: { message: self } }
 end
