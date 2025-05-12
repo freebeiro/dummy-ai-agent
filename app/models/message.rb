462ee9dce9e494b1d0217ad8447bd8c5885ca5dd
freebeiro <francisco.ribeiro@homeflow.co.uk>
2025-05-12 11:42:48 +0100

    Fix Turbo Stream target for message broadcasts

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :conversation
-  after_create_commit { broadcast_append_to conversation, target: "messages", partial: "messages/message", locals: { message: self } }
+  after_create_commit { broadcast_append_to conversation, target: "messages-container", partial: "messages/message", locals: { message: self } }
 end
